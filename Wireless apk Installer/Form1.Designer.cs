773b266fdb5de3a1aa0c64b9a51ab5cc9d125150
Laith Abusada <laithps4@hotmail.com>
2024-03-03 04:47:27 +0300

    Add project files.

---
+++ Wireless apk Installer/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿
+using System.Windows.Forms;
+namespace bulk_apk_installer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBoxIP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.materialMultiLineTextBox1 = new MaterialSkin.Controls.MaterialMultiLineTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(372, 114);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(121, 97);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(81)))), ((int)(((byte)(181)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(20, 211);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(124, 59);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Connect Device";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1Async);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(81)))), ((int)(((byte)(181)))));
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(180, 211);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(124, 59);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Disconnect Device";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBoxIP
+            // 
+            this.textBoxIP.Location = new System.Drawing.Point(37, 126);
+            this.textBoxIP.Name = "textBoxIP";
+            this.textBoxIP.Size = new System.Drawing.Size(154, 22);
+            this.textBoxIP.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(81)))), ((int)(((byte)(181)))));
+            this.label1.Location = new System.Drawing.Point(12, 129);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "IP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(81)))), ((int)(((byte)(181)))));
+            this.label2.Location = new System.Drawing.Point(369, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Connected Devices";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(81)))), ((int)(((byte)(181)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(20, 309);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 59);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Install Nice Apk";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // materialMultiLineTextBox1
+            // 
+            this.materialMultiLineTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.materialMultiLineTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.materialMultiLineTextBox1.Depth = 0;
+            this.materialMultiLineTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.materialMultiLineTextBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialMultiLineTextBox1.Hint = "";
+            this.materialMultiLineTextBox1.Location = new System.Drawing.Point(20, 387);
+            this.materialMultiLineTextBox1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialMultiLineTextBox1.Name = "materialMultiLineTextBox1";
+            this.materialMultiLineTextBox1.ReadOnly = true;
+            this.materialMultiLineTextBox1.Size = new System.Drawing.Size(473, 332);
+            this.materialMultiLineTextBox1.TabIndex = 11;
+            this.materialMultiLineTextBox1.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(81)))), ((int)(((byte)(181)))));
+            this.label3.Location = new System.Drawing.Point(208, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 16);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Port";
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.AcceptsTab = true;
+            this.textBoxPort.Location = new System.Drawing.Point(258, 126);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(59, 22);
+            this.textBoxPort.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(505, 731);
+            this.Controls.Add(this.textBoxPort);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.materialMultiLineTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxIP);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listView1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(2000, 2338);
+            this.MinimumSize = new System.Drawing.Size(348, 80);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Sizable = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bulk APK Installer";
+            this.TransparencyKey = System.Drawing.Color.Fuchsia;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ListView listView1;
+        private Button button3;
+        private Button button4;
+        private TextBox textBoxIP;
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private MaterialSkin.Controls.MaterialMultiLineTextBox materialMultiLineTextBox1;
+        private Label label3;
+        private TextBox textBoxPort;
+    }
+}
+
---
+++ Wireless apk Installer/Form1.cs
@@ -0,0 +1,305 @@
+﻿
+
+using MaterialSkin.Controls;
+using SharpAdbClient;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Windows.Forms;
+using WK.Libraries.BetterFolderBrowserNS;
+using System.Net;
+using System.Threading.Tasks;
+using AltoHttp;
+
+namespace bulk_apk_installer
+{
+    public partial class Form1 : MaterialForm
+    {
+
+        StreamWriter stdin = null;
+        String path = null;
+        String formname = "Wirless apk Installer";
+        String pathwithot = "";
+        int install_count = 0;
+        private Process cmdProcess;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = formname;
+
+        }
+
+        public void installadb()
+        {
+            AdbServer server = new AdbServer();
+            var result = server.StartServer("adb.exe", restartServerIfNewer: false);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            installadb();
+            loadtext();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+          //                 
+            
+        }
+
+
+        private void StartCmdProcess()
+        {
+            ProcessStartInfo pStartInfo = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                Arguments = "start /WAIT",
+                WorkingDirectory = AppDomain.CurrentDomain.BaseDirectory,
+                RedirectStandardOutput = true,
+                WindowStyle = ProcessWindowStyle.Hidden,
+                RedirectStandardError = true,
+                RedirectStandardInput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+
+            };
+
+             cmdProcess = new Process
+            {
+
+                StartInfo = pStartInfo,
+                EnableRaisingEvents = true,
+            };
+
+            cmdProcess.Start();
+            materialMultiLineTextBox1.Text = string.Empty;
+            cmdProcess.BeginErrorReadLine();
+            cmdProcess.BeginOutputReadLine();
+            stdin = cmdProcess.StandardInput;
+
+            cmdProcess.OutputDataReceived += (s, evt) =>
+            {
+                if (evt.Data != null)
+                {
+                    BeginInvoke(new MethodInvoker(() =>
+                    {
+                        materialMultiLineTextBox1.AppendText(evt.Data + Environment.NewLine);
+                        materialMultiLineTextBox1.ScrollToCaret();
+                    }));
+                }
+            };
+
+            cmdProcess.ErrorDataReceived += (s, evt) =>
+            {
+                if (evt.Data != null)
+                {
+                    BeginInvoke(new Action(() =>
+                    {
+                        //rtbStdErr.AppendText(evt.Data + Environment.NewLine);
+                        //rtbStdErr.ScrollToCaret();
+                    }));
+                }
+            };
+
+            cmdProcess.Exited += (s, evt) =>
+            {
+                // cmdProcess?.Dispose();
+            };
+        }
+
+     
+            
+            
+    
+
+        private void materialMultiLineTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                replace();
+
+                int maxLines = materialMultiLineTextBox1.Lines.Length;
+                string lastLine = materialMultiLineTextBox1.Lines[maxLines - 2];
+                string lastWord = lastLine.Split(' ').Last();
+
+                if (lastLine == "Success")
+                {
+                    install_count++;
+                    progresstext();
+                }
+            }
+            catch
+            {
+
+            }
+            
+        
+
+        }
+
+        
+        void progresstext()
+        {
+            materialMultiLineTextBox1.AppendText("----------------------------------------------" + Environment.NewLine+ "-- Installed number of apk "+install_count+" --" + Environment.NewLine + "----------------------------------------------" + Environment.NewLine);
+        }
+        void loadtext()
+        {
+            materialMultiLineTextBox1.AppendText("..... (¯`v´¯)♥" + Environment.NewLine + ".......•.¸.•´" + Environment.NewLine + "....¸.•´" + Environment.NewLine + "... (" + Environment.NewLine + "☻/" + Environment.NewLine + "/▌♥♥" + Environment.NewLine + "/ \u005C ♥♥" + Environment.NewLine );
+
+        }
+        void replace()
+        {
+            String Directory = System.Windows.Forms.Application.StartupPath;
+            materialMultiLineTextBox1.Text = materialMultiLineTextBox1.Text.Replace(Directory, "");
+            materialMultiLineTextBox1.Text = materialMultiLineTextBox1.Text.Replace(">", "");
+            materialMultiLineTextBox1.Text = materialMultiLineTextBox1.Text.Replace("for %e in", "");
+            materialMultiLineTextBox1.Text = materialMultiLineTextBox1.Text.Replace("%", "");
+            materialMultiLineTextBox1.Text = materialMultiLineTextBox1.Text.Replace("(", "");
+            materialMultiLineTextBox1.Text = materialMultiLineTextBox1.Text.Replace(path, "");
+            materialMultiLineTextBox1.Text = materialMultiLineTextBox1.Text.Replace("*.apk)", "");
+            materialMultiLineTextBox1.Text = materialMultiLineTextBox1.Text.Replace("do", "");
+            materialMultiLineTextBox1.Text = materialMultiLineTextBox1.Text.Replace("adb install", "");
+            materialMultiLineTextBox1.Text = materialMultiLineTextBox1.Text.Replace("@echo", "");
+            materialMultiLineTextBox1.Text = materialMultiLineTextBox1.Text.Replace("off", "");
+            materialMultiLineTextBox1.Text = materialMultiLineTextBox1.Text.Replace("-r", "");
+            materialMultiLineTextBox1.Text = materialMultiLineTextBox1.Text.Replace("\"%e\"", "");
+        }
+       
+
+        private async void button3_Click_1Async(object sender, EventArgs e)
+        {
+            string ipAddress = textBoxIP.Text;
+            int port = int.Parse(textBoxPort.Text);
+            string connect = "adb connect " + ipAddress + ":" + port;
+            MessageBox.Show("Connecting to device...");
+            StartCmdProcess();
+            try
+            {
+                stdin.Write("\u0040echo off" + Environment.NewLine);
+                stdin.Write("---------------------------------------------------------------" + Environment.NewLine + "-- Trying to establish connection --" + Environment.NewLine + "---------------------------------------------------------------" + Environment.NewLine);
+                stdin.Write(connect + Environment.NewLine);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            // Add a delay to allow time for the connection process
+            await Task.Delay(5000);
+
+            // Now check if the connection was successful by searching for the confirmation message
+            string[] lines = materialMultiLineTextBox1.Lines;
+            string searchWord = "connected"; // Update this with the confirmation message
+            bool wordFound = false;
+
+            foreach (string line in lines)
+            {
+                if (line.Contains(searchWord))
+                {
+                    // Word found in this line
+                    wordFound = true;
+                    break; // Exit the loop since we found the word
+                }
+            }
+
+            if (wordFound)
+            {
+                MessageBox.Show("Device connected successfully");
+                listView1.Items.Add(ipAddress + ':' + port);
+            }
+            else
+            {
+                MessageBox.Show("Device unable to connect, please check IP and port");
+            }
+
+            cmdProcess.CloseMainWindow();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string ipAddress = textBoxIP.Text;
+            int port = int.Parse(textBoxPort.Text);
+            string disconnect = "adb disconnect " + ipAddress + ":" + port;
+            StartCmdProcess();
+            try
+            {
+                stdin.Write("\u0040echo off" + Environment.NewLine);
+                stdin.Write("---------------------------------------------------------------" + Environment.NewLine + "-- Disconnecting Device --" + Environment.NewLine + "---------------------------------------------------------------" + Environment.NewLine);
+                stdin.Write(disconnect + Environment.NewLine);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            MessageBox.Show("Device disconnected successfully");
+            listView1.Items.Clear();
+            cmdProcess.CloseMainWindow();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            string websiteUrl = "https://innovo.net/repo/TP4/nice-latest.apk";
+            string downloadPath = "E:\\Wireless apk Installer\\Wireless apk Installer\\Downloads";
+            listView1.Items.Add("1.1.1.1:5555");
+
+            try
+            {
+                HttpDownloader httpDownloader;
+                httpDownloader = new HttpDownloader(websiteUrl,$"{Application.StartupPath}\\{Path.GetFileName("nice-latest.apk")}");
+                httpDownloader.Start();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error downloading file: {ex.Message}");
+            }
+
+            if (listView1.Items.Count == 0)
+            {
+                MessageBox.Show("No Device has connected to this PC, Please connect the device and press the refresh button", "Error..", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+
+
+            {
+                string path = "";
+                string installcom = $"for %%f in ({path}) do adb install -g -r -d %%f";
+
+
+                StartCmdProcess();
+
+                try
+                {
+                    stdin.Write("\u0040echo off" + Environment.NewLine);
+                    stdin.Write("---------------------------------------------------------------" + Environment.NewLine + "-- Wait a minute the installation started --" + Environment.NewLine + "---------------------------------------------------------------" + Environment.NewLine);
+                    stdin.Write(installcom + Environment.NewLine);
+
+                }
+                catch
+                {
+
+                }
+                finally
+                {
+                    cmdProcess.CloseMainWindow();
+                }
+
+
+
+            }
+        }
+
+ 
+    }
+}
